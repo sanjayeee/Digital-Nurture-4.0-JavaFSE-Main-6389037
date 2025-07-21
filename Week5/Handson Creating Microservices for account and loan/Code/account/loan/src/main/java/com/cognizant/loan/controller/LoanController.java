0f66e6bc45a9892d33a5722bb719c3d8e041046e
Sanjay <sanjaypetit@gmail.com>
2025-07-21 09:57:37 +0530

    Add files via upload

---
+++ Week5/Handson Creating Microservices for account and loan/Code/account/loan/src/main/java/com/cognizant/loan/controller/LoanController.java
@@ -0,0 +1,23 @@
+package com.cognizant.loan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class LoanController {
+
+    @GetMapping("/loans/{number}")
+    public Map<String, Object> getLoanDetails(@PathVariable String number) {
+        // Dummy response, hardcoded as per specification
+        return Map.of(
+            "number", number,
+            "type", "car",
+            "loan", 400000,
+            "emi", 3258,
+            "tenure", 18
+        );
+    }
+}
